fcbc67de95902099523fffeafa3a99c2ce93e8ad
Kero Youssef <krkryoussef00@gmail.com>
2026-05-06 17:26:28 +0300

    add auth

---
+++ controllers/authController.js
@@ -0,0 +1,88 @@
+    const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+// generate token
+const generateToken = (id) => {
+  return jwt.sign({ id }, "secretkey", {
+    expiresIn: "7d",
+  });
+};
+
+// ================== SIGNUP ==================
+exports.signup = async (req, res) => {
+try {
+    const { name, email, password } = req.body;
+
+    // check if user exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    res.status(201).json({
+      message: "User created successfully",
+      token: generateToken(user._id),
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// ================== LOGIN ==================
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check user
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    // compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    res.status(200).json({
+      message: "Login successful",
+      token: generateToken(user._id),
+      user,
+    });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// ================== FORGOT PASSWORD ==================
+exports.forgotPassword = async (req, res) => {
+  try {
+    const { email } = req.body;
+
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // مبدئيًا (simple version)
+    res.status(200).json({
+      message: "Reset password link sent (dummy for now)",
+    });
+
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
